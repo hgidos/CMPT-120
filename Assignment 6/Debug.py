665234af1826c8141ca1d3cae079a5f66312ee8f
hgidos <112525163+hgidos@users.noreply.github.com>
2022-11-10 11:14:56 -0500

    assignment 6

--- Assignment 6/Debug.py
+++ Assignment 6/Debug.py
@@ -3,47 +3,47 @@ class Dog:
     self.name = name
     self.age = age
 
+
 class Employee:
     def __init__(self, name, idNumber, department):
         self.name = name
         self.idNumber = idNumber
         self.department = department
+
         
 class Cake:
     def __init__(self, flavor, frosting):
         self.flavor = flavor
         self.frosting = frosting
-    #can you fill out the rest of this for me? im dumb
-    #the cake needs to have the cake flavor and cake frosting stored
+
 
 class Cat:
     def __init__(self, name, age, fur_length):
         self.name = name
         self.age = age
         self.fur_length = fur_length
-        
-        
+           
     def breedGuess(self):
         if self.fur_length == "long":
             return("Domestic Longhair")
         else:
             return("Domestic Shorthair")
+
             
 class Car:
     def __init__(self, model, year, color):
         self.model = model
         self.year = year
         self.color = color
+
     def carColor(self):
         if self.color == "yellow":
             return ("yellow car")
         else:
             return ("not yellow car")
-    #create your own function! what do you want it to do?
     
    
 def main():
-    #fill this one out with a dog's name and age.. can be your dog, friend's dog, etc
     dog1 = Dog("Thor", 2)
     print(dog1.name, dog1.age)
     
